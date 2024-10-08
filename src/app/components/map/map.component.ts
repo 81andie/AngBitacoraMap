0c3f5ed9aeccfb59dbda05ae13e4464db47b993d
81andie <133395577+81andie@users.noreply.github.com>
2024-10-08 19:15:25 +0200

    Pasando un proyecto de React a Angular

---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import Icon from 'ol/style/Icon';
+import Style from 'ol/style/Style';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+
+  private markers: number[][] = [];
+
+  private vectorSource: any = new VectorSource({
+    features: []
+  });
+
+  private vectorLayer = new VectorLayer({
+    source: this.vectorSource
+  });
+
+
+  ngOnInit(): void {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'map',
+      view: new View({
+        center: [311158.68373997946, 5157606.481663526],
+        zoom: 14,
+      }),
+    });
+
+    map.on('singleclick', (evt) => {
+      this.addMarker(evt.coordinate)
+      this.saveToLocalStorage()
+    });
+
+    map.addLayer(this.vectorLayer);
+
+    this.recoverMarkers()
+
+  }
+
+
+
+  private addMarker = (coordinate: number[]) => {
+
+    this.markers.push(coordinate)
+
+    const startMarker = new Feature({
+      type: 'point',
+      geometry: new Point(coordinate),
+    });
+
+    startMarker.setStyle(new Style({
+      image: new Icon({
+        anchor: [0.6, 0.5],
+        src: './assets/marker.png',
+        scale: 0.3
+      })
+    }))
+
+    this.vectorSource.addFeature(startMarker);
+
+    console.log('Marcadores:', this.markers);
+  }
+
+
+  saveToLocalStorage(): void {
+
+  let marcadores= this.markers;
+  localStorage.setItem('markers',JSON.stringify( marcadores));
+  }
+
+  recoverMarkers ():void{
+
+  const markersRecuperados = JSON.parse(localStorage.getItem('markers') || "[]");
+
+  markersRecuperados.forEach((coord:number[])=>{
+      this.addMarker(coord);
+  })
+
+  }
+
+}
